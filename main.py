a730ab9d76d7eb39b93c12f779c6362d02fde761
Ivan <kosenoki213@gmail.com>
2021-02-19 15:59:38 +0300

    Добавлена возможность менять масштаб карты

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from io import BytesIO
 import requests
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import Qt
 from PyQt5 import uic
 from PIL import Image
 
@@ -15,15 +16,38 @@ class MapMainWindow(QMainWindow):
 
     def initUI(self):
         uic.loadUi("design.ui", self)
+        self.update_image()
+
+    def update_image(self):
         binary_image = self.map_api_worker.get_image()
         self.image_container.setPixmap(QPixmap.fromImage(QImage.fromData(binary_image, "png")))
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            delta = self.map_api_worker.get_delta()
+            if delta < 72:
+                delta *= 1.25
+                self.map_api_worker.set_delta(delta)
+                self.update_image()
+        elif event.key() == Qt.Key_PageDown:
+            delta = self.map_api_worker.get_delta()
+            if delta > .0005:
+                delta /= 1.25
+                self.map_api_worker.set_delta(delta)
+                self.update_image()
 
 
 class MapAPIWorker:
     def __init__(self):
         self.longitude = 139.753882
         self.latitude = 35.6817
-        self.delta = .5
+        self.delta = .4
+
+    def get_delta(self):
+        return self.delta
+
+    def set_delta(self, value):
+        self.delta = value
 
     def get_image(self):
         map_api_server = "http://static-maps.yandex.ru/1.x/"
